75469ae2a5d73ebd08267974586245baed92b39d
Olga Mokhova <radus888@gmail.com>
2022-11-09 14:07:27 +0300

    Initial commit

---
+++ src/main/java/GetRequest.java
@@ -0,0 +1,32 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetRequest {
+    public static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
+
+            HttpGet request = new HttpGet(URL);
+
+            try(CloseableHttpResponse response = httpClient.execute(request)) {
+                List<Data> responseList = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+                responseList.stream()
+                        .filter(i -> i.getUpvotes() != null)
+                        .filter(i -> i.getUpvotes() != "0")
+                        .forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
